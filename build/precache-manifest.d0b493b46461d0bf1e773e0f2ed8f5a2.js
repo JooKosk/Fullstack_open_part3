c19907cde212b33a690aa841d091831bf69fda3c
Joonas Koskela <Jomppe.koskela@gmail.com>
2020-10-09 17:29:02 +0300

    frontend build commit

---
+++ build/precache-manifest.d0b493b46461d0bf1e773e0f2ed8f5a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "634f521cd377fc3eef5a0b82aaf2c7d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a15deb88bce7379561a",
+    "url": "/static/js/2.dbfacc78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dbfacc78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1abb1aa0df5dca516ef5",
+    "url": "/static/js/main.d3760007.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 morgan.token('reqData', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqData'))
